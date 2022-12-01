25da8d2bdb2752c48bed406994f2d0cbc57f5b7a
Aurélien <lefty.wolf.idk@hotmail.com>
2022-12-01 23:39:17 +0100

    Ajout du "squelette"
    Servira au fonctionnement du jeu, et pourra être repris lors des autres chapitres

--- src/Steps/Chapter1.php
+++ src/Steps/Chapter1.php
@@ -1,1 +1,28 @@
-start
+<!DOCTYPE html>
+<html lang="en">
+	
+	<?php
+		$rep;
+		$StartPoint;
+		$dialog=["base"=>[],
+		"bad"=>[], 
+		"good"=>[],
+		"med"=>[]
+		];
+		$branch="base";
+		$step=0;
+		$len=sizeof(dialog["base"])
+	
+	echo "<head><title>Game</title></head>";
+	echo "<body>";
+	
+	while(step<$len)
+		if(branch==""){
+			echo dialog{base[1]}
+		}
+	
+	
+	
+	echo "</body>";
+	?>
+</html>
